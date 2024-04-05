e4806fa961afabb3ddeb9cbc19aa5c43df28da66
coserei <70777715+coserei@users.noreply.github.com>
2024-04-05 19:47:40 +0200

    fixes

--- config.py
+++ config.py
@@ -1,19 +1,17 @@
-
 # Algemene variabelen
 
 vakGrootte = 16 # Geeft aan dat de grootte van elk vak in de game 16x16 is
 framerate = 60 # Bepaalt het aantal frame updates per seconde
-mapScale = 2 #
-slot = 0
-schermBreedte = 1000
-schermHoogte = 563
+mapScale = 2 # De schaalverhouding van de map
+slot = 0 # De geselecteerde slot bij het opstarten van de game
+schermBreedte = 1000 # De breedte van het scherm
+schermHoogte = 563 # De hoogte van het scherm
 
 # Lagen
 
 spelerLaag = 3 # De renderlaag waarin de speler zich bevindt
 borderLaag = 2 # De laag waarin de mapborders zich bevinden
 achtergrondLaag = 1 # De renderlaag waar de achtergrond zich in bevindt
-
 
 # Speler
 
@@ -24,5 +22,5 @@ playerSize = 4 # De grootte van de speler
 
 # Inventaris en items
 
-item = ["leeg", "aardbei", "tomaat"]
-guiScale = 4
+item = ["leeg", "aardbei", "tomaat"] # Alle items in de game
+guiScale = 4 # De schaalverhouding van het grafische interface (inventaris)
--- sprites.py
+++ sprites.py
@@ -9,17 +9,18 @@ i = 0
 huis1 = pygame.Rect(140 , 270, -175 , -200)
 veld1 = pygame.Rect(-160 , 315, -170 , -105)
 huis2 = pygame.Rect(-450 , 550, -175 , -200)
-
+deur1 = pygame.Rect(1000, 1000, -1000, -1000)
 
 # Lijst met hitboxes
 hitboxes = [huis1, veld1, huis2]
-interactableObjects = [huis1, huis2]
+interactableObjects = [huis1, huis2, deur1]
 
 class player(pygame.sprite.Sprite):
 
     def __init__(self, game):
         super().__init__()
 
+        self.interactableObjects = interactableObjects
         self.game = game
 
         self.speed = spelerSnelheid
@@ -27,11 +28,14 @@ class player(pygame.sprite.Sprite):
 
         self.charImg = []
 
-        self.charImg.append(pygame.image.load('img/char_l.png'))
-        self.charImg.append(pygame.image.load('img/char_r.png'))
+        self.charImg.append(pygame.image.load('img/char_l1.png'))
+        self.charImg.append(pygame.image.load('img/char_l2.png'))
+
+        self.charImg.append(pygame.image.load('img/char_r1.png'))
+        self.charImg.append(pygame.image.load('img/char_r2.png'))
         
         self.animatieFase = 0
-        self.img = pygame.transform.scale(self.charImg[round(self.animatieFase)], (playerSize*14, playerSize*41))
+        self.img = pygame.transform.scale(self.charImg[round(self.animatieFase)], (playerSize*17, playerSize*40))
 
         self.image = pygame.Surface((16, 16))
         self.image.fill((0, 0, 0))
@@ -43,19 +47,19 @@ class player(pygame.sprite.Sprite):
       self.velocity_y = 0
 
       keys = pygame.key.get_pressed()
-      
-      if keys[pygame.K_w]:
-        self.velocity_y = self.speed
-        self.spelerAnimatie()
-      if keys[pygame.K_a]:
-        self.velocity_x = self.speed
-        self.spelerAnimatie()
-      if keys[pygame.K_s]:
-        self.velocity_y = -self.speed
-        self.spelerAnimatie()
+
       if keys[pygame.K_d]:
         self.velocity_x = -self.speed
-        self.spelerAnimatie()
+        self.spelerAnimatie('rechts')
+      if keys[pygame.K_w]:
+        self.velocity_y = self.speed
+        self.spelerAnimatie('boven')
+      if keys[pygame.K_a]:
+        self.velocity_x = self.speed
+        self.spelerAnimatie('links')
+      if keys[pygame.K_s]:
+        self.velocity_y = -self.speed
+        self.spelerAnimatie('onder')
         #diagonaal bewegen moet even snel zijn, denk aan de 1, 1, wortel 2 driehoek
       if self.velocity_x != 0 and self.velocity_y != 0:
           self.velocity_x = self.velocity_x / math.sqrt(2)
@@ -94,36 +98,61 @@ class player(pygame.sprite.Sprite):
 
         keys = pygame.key.get_pressed()
 
-        for j in range(0, (len(interactableObjects) - 1), 1):
-            if pygame.Rect.colliderect(self.rect, interactableObjects[j]):
+        for self.interactCount in range(0, (len(self.interactableObjects) - 1), 1):
+            if pygame.Rect.colliderect(self.rect, self.interactableObjects[self.interactCount]):
                 break
             else:
-                j += 1
-        if pygame.Rect.colliderect(self.rect, interactableObjects[j]) and keys[pygame.K_e]:
-            print("hoi")
+              self.interactCount += 1
+        if pygame.Rect.colliderect(self.rect, self.interactableObjects[0]) and keys[pygame.K_e]:
+            self.achtergrond = pygame.image.load("img/huis.png")
+            self.hitboxes = self.hitboxeshuis
+            self.borders = self.borderhuis
+            self.pos[0] = 200
+            self.pos[1] = 200
+        if pygame.Rect.colliderect(self.rect, self.interactableObjects[1]) and keys[pygame.K_e]:
+            self.achtergrond = pygame.image.load("img/huis.png")
+            self.hitboxes = self.hitboxeshuis
+            self.borders = self.borderhuis
+            self.pos[0] = 200
+            self.pos[1] = 200
+        if pygame.Rect.colliderect(self.rect, self.interactableObjects[2]) and keys[pygame.K_q] and self.hitboxes == self.hitboxeshuis:
+            self.achtergrond = pygame.image.load("img/achtergrond.png")
+            self.hitboxes = self.hitboxesbuiten
+            self.borders = self.borderbuiten
+            self.pos[0] = 44
+            self.pos[1] = 10
 
-    def spelerAnimatie(self):
-       self.animeerSpeler = True
-
+    def spelerAnimatie(self, richting):
+        self.animeerSpeler = True
+        self.richting = richting
+       
     def update(self):
-      self.userInput()
-      self.move()
-      self.rect = pygame.Rect(self.pos[0], self.pos[1], 14, 41)
-      self.collideCheck()
-      self.interact()
-      i = 0
-      j = 0
-      self.bordercheck()
+        self.userInput()
+        self.move()
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], 14, 41)
+        self.collideCheck()
+        self.interact()
+        self.collideCount = 0
+        self.interactCount = 0
+        self.bordercheck()
 
-      if self.animeerSpeler == True:
-         self.animatieFase += 1
+        if self.animeerSpeler == True:
+            self.animatieFase += 0.0045
 
-         if self.animatieFase > 1:
-            self.animatieFase = 0
-            self.animeerSpeler = False
+            if self.animatieFase > 1 and self.richting == 'links':
+                self.animatieFase = 0
+                self.animeerSpeler = False
+            if self.animatieFase > 3 and self.richting == 'rechts':
+                self.animatieFase = 2
+                self.animeerSpeler = False
+            if self.animatieFase > 1 and self.richting == 'onder': # Moeten nog textures voor worden gemaakt
+                self.animatieFase = 0
+                self.animeerSpeler = False
+            if self.animatieFase > 1 and self.richting == 'boven': # Moeten nog textures voor worden gemaakt
+                self.animatieFase = 0
+                self.animeerSpeler = False
 
-      self.img = self.img = pygame.transform.scale(self.charImg[self.animatieFase], (playerSize*14, playerSize*41))
-      print(self.animatieFase)
+            self.img = pygame.transform.scale(self.charImg[round(self.animatieFase)], (playerSize*17, playerSize*40))
     
 #class crop:
       
